1f42d79438835db951686701108b83a252f0ebf7
Vivekvampyr <rajawatshivam55@gmail.com>
2026-09-11 17:26:09 +0530

    preparing backend for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set in .env")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=1, max_overflow=0, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
